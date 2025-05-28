f6c697d413bea36ed7dbc31a3eeb24d9c10da0eb
mobinapooyande <mobina.pouyandeh@gmail.com>
2025-05-29 00:12:55 +0330

    Update P4.cpp

--- P4.cpp
+++ P4.cpp
@@ -4,10 +4,17 @@
 using namespace std;
 void func(int i) {
   cout <<"hello"<< i << endl;}
+// threads one by one 
 int main() {
   array<thread,6> threads;
   for (int i = 0; i < 6; ++i) {
     threads[i]=thread(func, i + 1);}
   for (auto& th:threads) {
     th.join();}
+return 0;}
+//or threads in parallel
+int main() {
+ for (int i = 0; i < 5; i++) {
+     thread t0(func, i);
+     t0.join()}
 return 0;}
